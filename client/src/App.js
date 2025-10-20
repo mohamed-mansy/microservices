d039245040565ac057c6e06a0e4294bd7afebcff
mohamed-mansy <mohamedmanci0@gmail.com>
2025-10-20 22:44:48 +0300

    adding the files yaml of kubernetes cluster

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            we are using the latest version the best of the worst workarounds!
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
